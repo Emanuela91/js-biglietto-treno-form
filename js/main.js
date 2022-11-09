c1383d0262be22174734919ace378fc3422710c3
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-09 19:03:25 +0100

    html aggiunta parte da compilare, js aggiunti nome, km e costi biglietto minorenni

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+let element = document.getElementById("generate");
+element.addEventListener(`click`,
+
+    function() {
+        // Richiesta nome e cognome
+        let nome = document.getElementById("name").value;
+        
+        // Quanti Km
+        let km = document.getElementById("km").value;
+        
+        // Età
+        let eta = document.getElementById("eta").value;
+        
+        const costoKm = 0.21;
+        const minorenni = 0.8;
+        const anziano = 0.6;
+        
+        // Costo del biglietto: somma Km e costo al Km
+        costoBiglietto = km * costoKm;
+        
+        console.log(costoBiglietto);
+        
+        if (eta === "minorenne"){
+            totmin = costoBiglietto * minorenni;
+            console.log(totmin);
+        }
+
+        
+    }
+
+
+
+)
+
